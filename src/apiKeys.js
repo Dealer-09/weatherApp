40a30e319f2c4c9639e532ffb47e3578ea70b836
Archis <archismanpaul09@outlook.com>
2024-12-23 09:52:19 +0530

    Update apiKeys.js

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
+  key: "87fd3238b1408462c0d6d66cd73135f1",
   base: "https://api.openweathermap.org/data/2.5/",
 };
